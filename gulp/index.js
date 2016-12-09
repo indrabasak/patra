c5cdf7a7b6e8a29762cb030b453651a385b84617
Indra Basak <indra.basak1@gmail.com>
2016-12-09 15:17:44 -0800

    Initial revision.

---
+++ gulp/index.js
@@ -0,0 +1,183 @@
+// Gulp and plugins
+var gulp = require('gulp');
+var closureCompiler = require('gulp-closure-compiler');
+var header = require('gulp-header');
+var jsdoc = require("gulp-jsdoc");
+var jshint = require('gulp-jshint');
+var pkg = require('../package.json');
+var rename = require('gulp-rename');
+var rimraf = require('gulp-rimraf');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var testem = require('gulp-testem');
+var uglify = require('gulp-uglify');
+var util = require('gulp-util');
+var umd = require('gulp-umd');
+
+// paths
+var src = './src';
+var dist = './dist';
+var doc = './doc';
+var distJs = dist + '/js';
+var distCss = dist + '/css';
+var distFont = dist + '/fonts';
+var lib = './lib';
+var jsFiles = [src + '/js/' + 'patra.js'];
+var scssFiles = [src + '/scss/' + 'patra.scss'];
+var htmlFiles = [src + '/html/' + '*.html'];
+var libJsFiles = [lib + '/js/' + "*.js"];
+var libCssFiles = [lib + '/css/' + "**/*"];
+var libFontFiles = [lib + '/fonts/' + "**/*"];
+
+var banner = ['/**',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @author <%= pkg.author %>',
+    ' * @license <%= pkg.license %>',
+    ' */',
+    ''].join('\n');
+
+gulp.task('default', function (callback) {
+    runSequence('jshint', 'build:js', 'build:css', 'copy:html',
+        'copy:lib:js', 'copy:lib:css', 'copy:lib:fonts',
+        callback);
+});
+
+gulp.task('compile', function (callback) {
+    runSequence('jshint', 'compile:js', 'build:css', 'copy:html',
+        'copy:lib:js', 'copy:lib:css', 'copy:lib:fonts',
+        callback);
+});
+
+// deletes the distribution directory
+gulp.task('clean', function () {
+    return gulp.src([dist + '*'], {read: false})
+        .pipe(rimraf());
+});
+
+// create 'metricsgraphics.js' and 'metricsgraphics.min.js' from source js
+gulp.task('build:js', ['clean'], function () {
+    return gulp.src(jsFiles)
+        .pipe(umd(
+            {
+                dependencies: function () {
+                    return [{
+                        name: 'jquery',
+                        amd: 'jquery',
+                        cjs: 'jquery',
+                        global: 'jQuery',
+                        param: '$'
+                    },
+                        {
+                            name: 'MG',
+                            amd: 'MG',
+                            cjs: 'MG',
+                            global: 'MG',
+                            param: 'MG'
+                        },
+                        {
+                            name: 'jstree',
+                            amd: 'jstree',
+                            cjs: 'jstree',
+                            global: 'jstree',
+                            param: 'jstree'
+                        }];
+                },
+                exports: function () {
+                    return "patra";
+                },
+                namespace: function () {
+                    return "patra";
+                }
+            }
+        ))
+        .pipe(header(banner, {pkg: pkg}))
+        .pipe(gulp.dest(distJs))
+        .pipe(rename('patra.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(distJs));
+});
+
+gulp.task('compile:js', ['clean'], function () {
+    return gulp.src(jsFiles)
+        .pipe(gulp.dest(distJs))
+        .pipe(closureCompiler({
+            compilerPath: 'lib/closure/closure-compiler-v20161024.jar',
+            fileName: 'patra.min.js',
+            compilerFlags: {
+                compilation_level: 'ADVANCED_OPTIMIZATIONS',
+                warning_level: 'VERBOSE',
+                externs: [
+                    'lib/externs/jquery-3.1.js',
+                    'lib/externs/metricgraphics-2.10.1.js',
+                    'lib/externs/metricsviewer-1.0.0.js',
+                    'lib/externs/jstree-3.3.0.js'
+                ]
+            }
+        }))
+        .pipe(gulp.dest(distJs));
+});
+
+gulp.task('clean:doc', function () {
+    return gulp.src([doc + '*'], {read: false})
+        .pipe(rimraf());
+});
+
+gulp.task('doc', ['clean:doc'], function () {
+    return gulp.src(jsFiles)
+        .pipe(jsdoc(doc));
+});
+
+// build css files from scss
+gulp.task('build:css', function () {
+    return gulp.src(scssFiles)
+        .pipe(sass())
+        .pipe(gulp.dest(distCss));
+});
+
+gulp.task('copy:html', function () {
+
+    return gulp.src(htmlFiles)
+        .pipe(gulp.dest(dist))
+        .on('error', log);
+});
+
+gulp.task('copy:lib:js', function () {
+    return gulp.src(libJsFiles)
+        .pipe(gulp.dest(distJs))
+        .on('error', log);
+});
+
+gulp.task('copy:lib:css', function () {
+    return gulp.src(libCssFiles)
+        .pipe(gulp.dest(distCss))
+        .on('error', log);
+});
+
+
+gulp.task('copy:lib:fonts', function () {
+    return gulp.src(libFontFiles)
+        .pipe(gulp.dest(distFont))
+        .on('error', log);
+});
+
+
+// Check source js files with jshint
+gulp.task('jshint', function () {
+    return gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+    //.pipe(jshint.reporter('default'));
+});
+
+// Run the test suite
+gulp.task('test', function () {
+    return gulp.src([''])
+        .pipe(testem({
+            configFile: 'testem.json'
+        }));
+});
+
+function log(error) {
+    console.error(error.toString && error.toString());
+}
---
+++ src/js/patra.js
@@ -0,0 +1,415 @@
+(function (patra, $) {
+    'use strict';
+
+    patra.init = function (url) {
+        _url = url;
+        _keepRefreshing();
+    };
+
+    /**
+     * Called by the viewer to display a graph
+     * @param divId {string} div id where the graph will be displayed in the viewer
+     */
+    patra.addGraph = function (divId) {
+        if (_$selectedNode && _$selectedNode.data.key) {
+            var title = _$selectedNode.data.key;
+            var description = "";
+            var metricName = _$selectedNode.data.key;
+
+            if (_$selectedNode.data.type === METRIC_TYPE.COUNTER) {
+                metricsViewer.addCounter(divId, title, description, metricName);
+            } else if (_$selectedNode.data.type === METRIC_TYPE.GAUGE) {
+                metricsViewer.addGauge(divId, title, description, metricName);
+            } else if (_$selectedNode.data.type === METRIC_TYPE.METER) {
+                if (_$selectedNode.data.value) {
+                    title = title + "." + _$selectedNode.text;
+                    metricsViewer.addMeterWithProperty(divId, title, description,
+                        metricName, _$selectedNode.text);
+                } else {
+                    metricsViewer.addMeter(divId, title, description, metricName);
+                }
+            } else if (_$selectedNode.data.type === METRIC_TYPE.TIMER) {
+                if (_$selectedNode.data.value) {
+                    title = title + "." + _$selectedNode.text;
+                    metricsViewer.addTimerWithProperty(divId, title, description,
+                        metricName, _$selectedNode.text);
+                } else {
+                    metricsViewer.addTimer(divId, title, description, metricName);
+                }
+            }
+
+            metricsViewer.init();
+        }
+    };
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    // PRIVATE FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Types of metrics
+     * @enum {Object}
+     *
+     */
+    var METRIC_TYPE = {
+        COUNTER: {type: "counters"},
+        GAUGE: {type: "gauges"},
+        METER: {type: "meters"},
+        TIMER: {type: "timers"}
+    };
+    Object.freeze(METRIC_TYPE);
+
+    /**
+     * The div id of the left panel where the tree is displayed
+     * @type {string}
+     */
+    var _treedId = '#jstree-div';
+    Object.freeze(_treedId);
+
+    /**
+     * The div id of the right panel where the metrics graph is displayed
+     * @type {string}
+     */
+    var _viewerId = '#trunk-div';
+    Object.freeze(_viewerId);
+
+    /**
+     * CSS class of the input search field
+     * @type {string}
+     */
+    var _searchClass = '.input-tree';
+    Object.freeze(_searchClass);
+
+    var _url;
+
+    /**
+     * Map which hold the tree node paths. The key holds the node path, while the value hold the parent path.
+     * Avoids insertion of duplicate tree nodes
+     * @type {Map}
+     */
+    var _metricsMap = new Map();
+
+    /**
+     * The root of the metric tree
+     */
+    var _$root;
+
+    /**
+     * The selected tree node
+     */
+    var _$selectedNode;
+
+    //set the active viewer and section on first load/
+    /**
+     @type {string}
+     */
+    var currentPage = (document.location.hash) ? document.location.hash.slice(1) : 'patra-tree';
+
+    $('#trunk').load(currentPage + '.html', function () {
+        if (currentPage === "patra-tree") {
+            _initializeTree();
+        }
+    });
+
+    $('.docs li a#goto-' + currentPage).addClass('active');
+
+    /**
+     * Listens to events generated from clicks on navigation links.
+     * Displays the page (metric tree or JVM metrics) based on the link clicked.
+     * The link clicked is made active.
+     */
+    var _assignEventListeners = function () {
+        $('ul.docs li a.viewer').on('click', function (event) {
+            event.preventDefault();
+
+            //remove the active viewer from the trunk div
+            $('ul.docs li a.viewer').removeClass('active');
+            $(this).addClass('active');
+
+            //clear the old div tags
+            metricsViewer.clear();
+
+            //get the name of the viewer, stars after 'goto-' phrase
+            var currentPage = /** @type {string} */ ($(this).attr('id').slice(5));
+            $('#trunk').load(currentPage + '.html', function () {
+                if (currentPage === "patra-tree") {
+                    _initializeTree();
+                }
+            });
+
+            document.location.hash = currentPage;
+
+            return false;
+        });
+    };
+
+    //handle mouse clicks and so on
+    _assignEventListeners();
+
+    /**
+     * Calls the tree _refresh method once the page is read. It makes calls to fetch metric data
+     * from the server
+     */
+    // $(function () {
+    //     _keepRefreshing();
+    // });
+
+    /**
+     * Loads the viewer page when it's ready
+     */
+    $(_viewerId).load('patra-viewer.html', function () {
+    });
+
+    /**
+     * Initializes the metric tree. Clears the metric map before creating a new tree.
+     */
+    var _initializeTree = function () {
+        _metricsMap.clear();
+        metricsViewer.clear();
+
+        _$root = $(_treedId).jstree({
+            "core": {
+                check_callback: true
+            },
+            "themes": {},
+            "ui": {},
+            "types": {
+                "key": {
+                    "icon": "glyphicon glyphicon-stats icon-blue"
+                },
+                "value": {
+                    "icon": "glyphicon glyphicon-stats icon-green"
+                },
+                "value-str": {
+                    "icon": "glyphicon glyphicon-leaf icon-green"
+                },
+                "default": {}
+            },
+            "plugins": ["search", "sort", "wholerow", "types"]
+        });
+
+        /**
+         * Displays a corresponding metrics graph based on node selected
+         */
+        $(_treedId).on('select_node.jstree', function (e, data) {
+            _displayViewer(data);
+        });
+
+        /**
+         * Filters the tree node based on input search text
+         */
+        $(_searchClass).keyup(function () {
+            var searchString = $(this).val();
+            $(_treedId).jstree('search', searchString);
+        });
+    };
+
+    /**
+     * Adds a metric root node based on metric type
+     * @param metricType {METRIC_TYPE} metricType the type of metric class
+     * @param jsonData {string} metric data in json format
+     */
+    var _addMetricTree = function (metricType, jsonData) {
+        var jsonNode = jsonData[metricType.type];
+        var typeId = metricType.type;
+        if (!_metricsMap.has(typeId)) {
+            _addNode(metricType, "#", typeId, metricType.type, false, false, false);
+            _metricsMap.set(typeId, "#");
+        }
+
+        var parentId;
+        var id = typeId;
+        $.each(jsonNode, function (key, val) {
+            id = typeId;
+            var tokens = key.split(".");
+            for (var i = 0; i < tokens.length; i++) {
+                parentId = id;
+                id = id + "." + tokens[i];
+                if (!_metricsMap.has(id)) {
+                    if (i === (tokens.length - 1)) {
+                        _addNode(metricType, parentId, id, tokens[i], true, false, false);
+                    } else {
+                        _addNode(metricType, parentId, id, tokens[i], false, false, false);
+                    }
+                    _metricsMap.set(id, parentId);
+                }
+            }
+
+            parentId = id;
+            $.each(val, function (k, v) {
+                id = parentId + "." + k;
+                if (!_metricsMap.has(id)) {
+                    var numeric = $.isNumeric(v);
+                    _addNode(metricType, parentId, id, k, false, true, numeric);
+                    _metricsMap.set(id, parentId);
+                }
+            });
+        });
+    };
+
+    /**
+     * Adds a metric tree node
+     * @param metricType {METRIC_TYPE} metricType the type of metric class
+     * @param parentId {string} node id (metric path) of the parent node
+     * @param childId {string} id of the node to be added
+     * @param childTxt {string} name of the child node
+     * @param key {boolean} true if the node can be displayed in a metric graph
+     * @param metricValue {boolean} true if the node represents a metric value
+     * @param numeric {boolean} true if the value is numeric
+     */
+    var _addNode = function (metricType, parentId, childId, childTxt, key, metricValue, numeric) {
+        var $parentNode = _$root.jstree(true).get_node(parentId);
+        var childData;
+        var data;
+        if (metricValue) {
+            if (numeric) {
+                childData = {"id": childId, "text": childTxt, "type": "value"};
+            } else {
+                childData = {"id": childId, "text": childTxt, "type": "value-str"};
+            }
+            data = {"type": metricType, "key": _retrieveKey(metricType, parentId), "value": true};
+        } else {
+            if (key) {
+                childData = {"id": childId, "text": childTxt, "type": "key"};
+                data = {"type": metricType, "key": _retrieveKey(metricType, childId), "value": false};
+            } else {
+                childData = {"id": childId, "text": childTxt};
+                data = {"type": metricType, "key": null, "value": false};
+            }
+        }
+
+        _$root.jstree(true).create_node($parentNode, childData);
+        var $node = _$root.jstree(true).get_node(childId);
+        $node.data = data;
+    };
+
+    /**
+     * Creates the key to retrieve the json node from the metric data.
+     * The metric class name is stripped from the path
+     * @param metricType {METRIC_TYPE} metricType the type of metric class
+     * @param path the path of the tree node
+     * @returns {string} the metric key
+     */
+    var _retrieveKey = function (metricType, path) {
+        var key = "";
+
+        if (metricType === METRIC_TYPE.COUNTER) {
+            key = path.substring(metricType.type.length + 1);
+        } else if (metricType === METRIC_TYPE.GAUGE) {
+            key = path.substring(metricType.type.length + 1);
+        } else if (metricType === METRIC_TYPE.METER) {
+            key = path.substring(metricType.type.length + 1);
+        } else if (metricType === METRIC_TYPE.TIMER) {
+            key = path.substring(metricType.type.length + 1);
+        }
+
+        return key;
+    };
+
+    /**
+     * Refreshes the metric tree and graph after every 15 sec
+     */
+    var _keepRefreshing = function () {
+        _refresh();
+        setInterval(_refresh, 2000);
+    };
+
+    /**
+     * Makes an ajax call to fetch metrics
+     */
+    var _refresh2 = function () {
+        //var url = "metrics/metrics";
+        var url = "http://localhost:8080/metrics/metrics";
+        //$.support.cors = true;
+        $.ajax({
+            // contentType: "application/json",
+            url: url,
+            method: 'GET',
+            dataType: "json",
+            cache: false,
+            headers: {
+                //accept: 'application/json; charset=UTF-8',
+                'Cache-Control': 'max-age=1000'
+            },
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            success: function (data) {
+                _updatePage(data);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log("Error - " + xhr.status + "Message: " + thrownError);
+            },
+            async: true
+        });
+    };
+
+    var _refresh = function () {
+        //var url = 'http://localhost:8080/metrics/metrics';
+        var url = _url;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.setRequestHeader('accept', 'application/json; charset=UTF-8');
+        xhr.withCredentials = false;
+        xhr.onload = function () {
+            if (this.status == 200) {
+                if (this.responseText && !_isEmpty(this.responseText)) {
+                    try {
+                        var jsonResponse = JSON.parse(this.responseText);
+                        _updatePage(jsonResponse);
+                    } catch (err) {
+                    }
+                }
+            }
+        };
+        xhr.onreadystatechange = function () {
+            if (this.status == 200) {
+                if (this.responseText && !_isEmpty(this.responseText)) {
+                    try {
+                        var jsonResponse = JSON.parse(this.responseText);
+                        _updatePage(jsonResponse);
+                    } catch (err) {
+                    }
+                }
+            }
+        };
+        xhr.send();
+    };
+
+    /**
+     * Test method to fetch metric data from a file
+     */
+    var _refresh1 = function () {
+        $.getJSON('../data/metrics.json', function (data) {
+            _updatePage(data);
+        });
+    };
+
+    var _updatePage = function (data) {
+        metricsViewer.refresh(data);
+        if (_$root) {
+            _addMetricTree(METRIC_TYPE.COUNTER, data);
+            _addMetricTree(METRIC_TYPE.GAUGE, data);
+            _addMetricTree(METRIC_TYPE.METER, data);
+            _addMetricTree(METRIC_TYPE.TIMER, data);
+        }
+    };
+
+    /**
+     * Displays metric graph based on the selected tree node
+     * @param data
+     */
+    var _displayViewer = function (data) {
+        _$selectedNode = data.node;
+        metricsViewer.clear();
+
+        $(_viewerId).load('patra-viewer.html', function () {
+        });
+    };
+
+    var _isEmpty = function (str) {
+        return (!str || 0 === str.length);
+    };
+
+}(window.patra = window.patra || {}, jQuery));
